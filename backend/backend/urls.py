6c75703d37da81051fe499fe7a52520c48b8616f
Jose Luis Caceres Losada <jlcaclosada@gmail.com>
2025-11-04 15:21:01 +0100

    deploy

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,8 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.http import JsonResponse
+
+def healthcheck(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path('api/', healthcheck),
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/games/', include('games.urls')),
