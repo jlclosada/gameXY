9c6f4acc0fd081710f209d093e39aba5bb994a37
Jose Luis Caceres Losada <jlcaclosada@gmail.com>
2025-11-04 15:38:07 +0100

    test

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,7 +9,7 @@ def healthcheck(request):
     return JsonResponse({"status": "ok"})
 
 urlpatterns = [
-    path('api/', healthcheck),
+    path('', healthcheck),
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/games/', include('games.urls')),
